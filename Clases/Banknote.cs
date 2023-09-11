aed666e8532f899512f3cfc18c2af632a528758d
Amir <galiev.amir@gmail.com>
2023-09-12 01:08:15 +0700

    init

---
+++ Clases/Banknote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Clases
+{
+    public class Banknote
+    {
+        public int denom { get; set; }
+        public int current { get; set; }
+        public int max {  get; set; }
+
+    }
+}
---
+++ Clases/Handler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Clases
+{
+    public class Handler
+    {
+        List<Banknote> banknoteList;
+
+    }
+}
